573e754f535a03b5ae006f1407d13b0bef63162c
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-15 10:59:35 +0300

    Completed assignment

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,14 @@
 class Course:
-    pass
+    def __init__(self, title, schedule, description):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output=""
+        output += f'Title: {self.title}\n Schedule: {self.schedule}\nDescription: {self.description}\n'
+        output +='-----------------------------'
+        return output   
+    
+
+#programming = Course("Programming Robots for Outer Space,", "FullTime", "Learn python programming in 15 weeks")
